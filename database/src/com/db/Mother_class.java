fdf4e52e035b982f36562683fedc691a333daef4
sungu1516 <76841523+sungu1516@users.noreply.github.com>
2021-05-02 23:48:44 +0900

    db

---
+++ database/src/com/db/Mother_class.java
@@ -0,0 +1,13 @@
+package com.db;
+
+public class Mother_class {
+	// 데이터 관련 메서드
+	String path_id = "A:\\Swing\\database\\User_info\\user_id.txt";
+	String path_idpw = "A:\\Swing\\database\\User_info\\user_idpw.txt";
+	String path_id_info = "A:\\Swing\\database\\User_info\\user_info.txt";
+	
+	
+	
+	
+	
+}
--- database/src/com/db/Print.java
+++ database/src/com/db/Print.java
@@ -1,12 +1,20 @@
 package com.db;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import com.db.Database;
 
 public class Print {
 
 	public static void main(String[] args) {
+
+		Sign_up a = new Sign_up("73532902", 1234);
+		Sign_up c = new Sign_up("53623442", 9834);
+		Sign_up d = new Sign_up("63543902", 1235);
+		Sign_up e = new Sign_up("15339402", 8888);
 		
-		Sign_up user2 = new Sign_up("testid2", 5678, "테스트", "서울특별시");
+		
 		
 	}
 
